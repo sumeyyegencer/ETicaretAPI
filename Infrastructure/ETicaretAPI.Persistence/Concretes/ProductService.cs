fbe8e2bfa1cd3a76312a5a724e36c5bc4cd9223b
sumeyyegencer <sumeyyegencer@gmail.com>
2022-06-14 23:15:58 +0300

    Add project files.

---
+++ Infrastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,19 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Concrete
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProduct()
+         => new() 
+         {
+         
+         };
+    }
+}
