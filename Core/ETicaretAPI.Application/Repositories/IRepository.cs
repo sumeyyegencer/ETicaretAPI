71cca30468f0ed96a38c45cb2ab65b08c38228a9
sumeyyegencer <sumeyyegencer@gmail.com>
2022-06-21 00:58:07 +0300

    Added project file.

--- Core/ETicaretAPI.Application/Repositories/IRepository.cs
+++ Core/ETicaretAPI.Application/Repositories/IRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace ETicaretAPI.Application.Repositories
 {
-    public interface IRepository
+    public interface IRepository<T> where T:class
     {
+        DbSet<T> Table { get; }
     }
 }
--- Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
+++ Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace ETicaretAPI.Application.Repositories
 {
-    public interface IWriteRepository
+    public interface IWriteRepository<T>:IRepository<T> where T :class   
     {
+        Task<bool> AddAsync(T model);
+        Task<bool> AddAsync(List<T> model);
+        Task<bool> Remove(T model);
+        Task<bool> Remove(string id);
+        Task<bool> UpdateAsync(T model);
+
     }
 }
