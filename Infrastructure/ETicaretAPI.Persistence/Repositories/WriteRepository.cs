54655f7c358b973b72fa16d714815b4149ed36ff
sumeyyegencer <sumeyyegencer@gmail.com>
2022-06-24 16:04:48 +0300

    Added project.

---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/WriteRepository.cs
@@ -0,0 +1,48 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities.Common;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories
+{
+    public class WriteRepository<T> : IWriteRepository<T> where T : BaseEntity
+    {
+        private readonly ETicaretAPIDbContext _context;
+
+        public WriteRepository(ETicaretAPIDbContext context)
+        {
+            _context = context;
+        }
+        public DbSet<T> Table => _context.Set<T>();
+
+        public Task<bool> AddAsync(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AddRangeAsync(List<T> model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Remove(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Remove(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
